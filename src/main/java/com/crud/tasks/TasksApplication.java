eb1f4fca8323c567fa61763737788db3e3d01ddc
“x-raf <x-raf@wp.pl>
2018-08-22 16:52:17 +0200

    Submodule 19.2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -7,13 +7,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-//public class TasksApplication extends SpringBootServletInitializer {
-public class TasksApplication {
+public class TasksApplication extends SpringBootServletInitializer {
+//public class TasksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
-	/*@Override
+	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(TasksApplication.class);
-	}*/
+	}
 }

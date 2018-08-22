ac0bc1c30fb35cfb86d0cc7f585b7bdc772c66a3
“x-raf <x-raf@wp.pl>
2018-08-23 00:29:37 +0200

    Submodule 19.4 Working mail service

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -33,7 +33,7 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        if (mail.getToCc() != null) {
+        if (mail.getToCc() != null && !mail.getToCc().equals("")) {
             mailMessage.setCc(mail.getToCc());
         }
         return mailMessage;
